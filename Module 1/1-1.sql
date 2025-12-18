9e07b2cc7ba55013801e2a57ef116282d98b44ab
Ameyakarale <ameyakarale@gmail.com>
2025-12-18 14:41:00 +0530

    Select statement and clause

---
+++ Module 1/1-1.sql
@@ -0,0 +1,20 @@
+
+-- Select statement
+SELECT *
+FROM classicmodels.employees
+WHERE officeCode = 1
+ORDER BY firstname;
+
+-- Select only a few columns
+SELECT firstname,lastname
+FROM classicmodels.employees
+WHERE officecode = 1
+ORDER BY firstname
+
+-- Select caluse
+SELECT  productCode,
+		productName,
+        buyPrice,
+        MSRP as "Selling Price",
+        (MSRP*0.90) as "Discounted Price"
+FROM products;
