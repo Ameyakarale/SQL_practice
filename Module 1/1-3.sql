bbcbd3cb1cf01bc993647b2f27c834c3568dd6e0
Ameyakarale <ameyakarale@gmail.com>
2025-12-24 12:56:07 +0530

    Logic Operators

--- Module 1/1-3.sql
+++ Module 1/1-3.sql
@@ -1,6 +1,19 @@
--- LOGIC OPERATORS
+-- LOGIC OPERATORS AND, OR, NOT
+
+-- AND
 SELECT *
 FROM payments
 WHERE (amount >= 40000 AND amount <= 60000);
 
--- 
+-- OR
+SELECT *
+FROM payments
+WHERE (amount <= 40000 OR amount >= 60000);
+
+-- Query: payment >= 2005-05-25
+
+SELECT *
+FROM payments
+WHERE (amount <= 40000 OR amount >= 60000)
+		AND paymentDate >= 2005-05-25
+ORDER BY (paymentDate);
